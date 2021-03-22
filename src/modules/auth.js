89a01afbede0dc40139423589405e799b2713c5b
twozeronine <amdusias209@gmail.com>
2021-03-22 15:14:29 +0900

    auth 모듈 작성

--- src/modules/auth.js
+++ src/modules/auth.js
@@ -1,14 +1,49 @@
 import { createAction, handleActions } from "redux-actions";
+import produce from "immer";
 
-const SAMPLE_ACTION = "auth/SAMPLE_ACTION";
+// 액션 타입 설정
 
-export const sampleAction = createAction(SAMPLE_ACTION);
+const CHANGE_FIELD = "auth/CHANGE_FIELD";
+const INITIALIZE_FORM = "auth/INITIALIZE_FORM";
 
-const initialState = {};
+// 액션 객체 생성 함수
+export const changeField = createAction(
+  CHANGE_FIELD,
+
+  ({ form, key, value }) => ({
+    form, // register , login
+    key, // username , password, passwordConfirm
+    value, // 실제 바꾸려는 값
+  }),
+);
+
+export const initalizeForm = createAction(INITIALIZE_FORM, (form) => form); //register / login
+
+// 초기 상태 설정
+const initialState = {
+  register: {
+    username: "",
+    password: "",
+    passwordConfirm: "",
+  },
+  login: {
+    username: "",
+    password: "",
+  },
+};
+
+// 리듀서 함수
 
 const auth = handleActions(
   {
-    [SAMPLE_ACTION]: (state, action) => state,
+    [CHANGE_FIELD]: (state, { payload: { form, key, value } }) =>
+      produce(state, (draft) => {
+        draft[form][key] = value; // 예: state.register.username을 바꾼다.
+      }),
+    [INITIALIZE_FORM]: (state, { payload: form }) => ({
+      ...state,
+      [form]: initialState[form],
+    }),
   },
   initialState,
 );
