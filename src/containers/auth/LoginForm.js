04fb69de0d3b99f57033bb9f6c6b3682422f0c11
twozeronine <amdusias209@gmail.com>
2021-03-23 11:28:04 +0900

    회원 인증 에러 처리하기

--- src/containers/auth/LoginForm.js
+++ src/containers/auth/LoginForm.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { withRouter } from "react-router-dom";
 import { changeField, initializeForm, login } from "../../modules/auth";
@@ -6,6 +6,9 @@ import AuthForm from "../../components/auth/AuthForm";
 import { check } from "../../modules/user";
 
 const LoginForm = ({ history }) => {
+  // 에러 처리를 위한 상태
+  const [error, setError] = useState(null);
+
   const dispatch = useDispatch();
   const { form, auth, authError, user } = useSelector(({ auth, user }) => ({
     form: auth.login,
@@ -40,6 +43,7 @@ const LoginForm = ({ history }) => {
     if (authError) {
       console.log("오류 발생");
       console.log(authError);
+      setError("로그인 실패");
       return;
     }
     if (auth) {
@@ -60,6 +64,7 @@ const LoginForm = ({ history }) => {
       form={form}
       onChange={onChange}
       onSubmit={onSubmit}
+      error={error}
     />
   );
 };
--- src/containers/auth/RegisterForm.js
+++ src/containers/auth/RegisterForm.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { changeField, initializeForm, register } from "../../modules/auth";
 import AuthForm from "../../components/auth/AuthForm";
@@ -6,6 +6,7 @@ import { check } from "../../modules/user";
 import { withRouter } from "react-router-dom";
 
 const RegisterForm = ({ history }) => {
+  const [error, setError] = useState(null);
   const dispatch = useDispatch();
   const { form, auth, authError, user } = useSelector(({ auth, user }) => ({
     form: auth.register,
@@ -28,8 +29,18 @@ const RegisterForm = ({ history }) => {
   const onSubmit = (e) => {
     e.preventDefault();
     const { username, password, passwordConfirm } = form;
+    // 하나라도 비어 있다면
+    if ([username, password, passwordConfirm].includes("")) {
+      setError("빈 칸을 모두 입력하세요.");
+      return;
+    }
+    // 비밀번호가 일치하지 않는다면
     if (password !== passwordConfirm) {
-      // TODO: 오류처리
+      setError("비밀번호가 일치하지 않습니다.");
+      dispatch(changeField({ form: "register", key: "password", value: "" }));
+      dispatch(
+        changeField({ form: "register", key: "passwordConfirm", value: "" }),
+      );
       return;
     }
     //register 액션은 리듀서에서 처리되지않고 saga에서 처리됨
@@ -44,8 +55,13 @@ const RegisterForm = ({ history }) => {
   // 회원가입 성공 / 실패 처리
   useEffect(() => {
     if (authError) {
-      console.log("오류 발생");
-      console.log(authError);
+      // 계정명이 이미 존재할 때
+      if (authError.response.status === 409) {
+        setError("이미 존재하는 계정명입니다.");
+        return;
+      }
+      // 기타 이유
+      console.log("회원가입 실패");
       return;
     }
     if (auth) {
@@ -68,6 +84,7 @@ const RegisterForm = ({ history }) => {
       form={form}
       onChange={onChange}
       onSubmit={onSubmit}
+      error={error}
     />
   );
 };
