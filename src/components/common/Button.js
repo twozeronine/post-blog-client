8f709483fe622e5a9b5e74a9244fa00e2acad8d3
twozeronine <amdusias209@gmail.com>
2021-03-23 11:57:09 +0900

    로그인 버튼을 누를시 해당 라우터로 이동 구현 첫번째 방법

--- src/components/common/Button.js
+++ src/components/common/Button.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styled, { css } from "styled-components";
+import { withRouter } from "react-router-dom";
 import palette from "../../lib/styles/palette";
 
 const StyledButton = styled.button`
@@ -36,6 +37,16 @@ const StyledButton = styled.button`
     `}
 `;
 
-const Button = (props) => <StyledButton {...props} />;
-
-export default Button;
+const Button = ({ to, history, ...rest }) => {
+  const onClick = (e) => {
+    // to가 있다면 to로 페이지 이동
+    if (to) {
+      history.push(to);
+    }
+    if (rest.onClick) {
+      rest.onClick(e);
+    }
+  };
+  return <StyledButton {...rest} onClick={onClick} />;
+};
+export default withRouter(Button);
