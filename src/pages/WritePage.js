ee291b479459557d8c253baa350343fcec46dd89
twozeronine <amdusias209@gmail.com>
2021-03-24 12:52:33 +0900

    포스트 등록하기 구현

--- src/pages/WritePage.js
+++ src/pages/WritePage.js
@@ -1,15 +1,15 @@
 import React from "react";
-import WriteActionButtons from "../components/write/WriteActionButtons";
 import Responsive from "../components/common/Responsive";
 import EditorContainer from "../containers/write/EditorContainer";
 import TagBoxConainer from "../containers/write/TagBoxContainer";
+import WriteActionButtonsContainer from "../containers/write/WriteActionButtonsContainer";
 
 const WritePage = () => {
   return (
     <Responsive>
       <EditorContainer />
       <TagBoxConainer />
-      <WriteActionButtons />
+      <WriteActionButtonsContainer />
     </Responsive>
   );
 };
